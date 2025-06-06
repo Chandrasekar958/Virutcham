5c8e1e2ebbf1e1c45a6ad03504cd5c8f2b0ade01
Chandrasekar958 <chandru958@gmail.com>
2025-06-07 01:22:59 +0530

    Create Products.js

---
+++ Products.js
@@ -0,0 +1,74 @@
+const products = [
+  {
+    id: "briefs",
+    name: "Men’s Briefs",
+    price: 45,
+    description: "Comfortable and durable men's briefs.",
+    image: "images/briefs.jpg",
+    minQty: 1,
+    maxQty: 123
+  },
+  {
+    id: "trunks",
+    name: "Men’s Trunks",
+    price: 70,
+    description: "Stylish men's trunks for everyday wear.",
+    image: "images/trunks.jpg",
+    minQty: 1,
+    maxQty: 123
+  },
+  {
+    id: "white-vest",
+    name: "Men’s White Vest",
+    price: 60,
+    description: "Classic white vest for comfort.",
+    image: "images/white-vest.jpg",
+    minQty: 1,
+    maxQty: 123
+  },
+  {
+    id: "white-vest-short",
+    name: "Men’s White Vest Short Sleeves",
+    price: 75,
+    description: "White vest with short sleeves.",
+    image: "images/white-vest-short.jpg",
+    minQty: 1,
+    maxQty: 123
+  },
+  {
+    id: "colour-vest",
+    name: "Men’s Colour Vest",
+    price: 65,
+    description: "Colourful vest options.",
+    image: "images/colour-vest.jpg",
+    minQty: 1,
+    maxQty: 123
+  },
+  {
+    id: "shorts",
+    name: "Men’s Shorts",
+    price: 199,
+    description: "Comfortable men's shorts.",
+    image: "images/shorts.jpg",
+    minQty: 1,
+    maxQty: 123
+  },
+  {
+    id: "cotton-towels",
+    name: "Cotton Towels",
+    price: 50,
+    description: "Soft and absorbent cotton towels.",
+    image: "images/cotton-towels.jpg",
+    minQty: 1,
+    maxQty: 123
+  },
+  {
+    id: "cotton-bedsheets",
+    name: "Cotton Bedsheets",
+    price: 140,
+    description: "Comfortable cotton bedsheets.",
+    image: "images/cotton-bedsheets.jpg",
+    minQty: 1,
+    maxQty: 123
+  }
+];
